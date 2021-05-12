e4f6e92a3c9d0db6538252d23860b6420fb96509
Vinay Gupta <vinaygupta@Vinays-Mac.local>
2021-05-12 11:05:38 +0530

    set up the ejs

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const port = 8000;
 //use express router here
 app.use('/', require('./routes')); // app.use('/', require('./routes/index')); are same
 
+//setting up the view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.listen(port, function(err) {
     if(err) {
         console.log(`Error in running the Server : ${err}`);
