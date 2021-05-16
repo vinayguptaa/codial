dcc3c44c7e48c32526b5bcc6ee75facdc20b55eb
Vinay Gupta <vinaygupta@Vinays-Mac.local>
2021-05-16 20:37:03 +0530

    add static files

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+
 app.use(expressLayouts);
+
+//extract style and scripts from subpages to the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 //use express router here
 app.use('/', require('./routes')); // app.use('/', require('./routes/index')); are same
